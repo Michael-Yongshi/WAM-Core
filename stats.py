d09903c3aa53c21bb46baac01ada27a4461ace03
MvdBerg <mfvandenberg@outlook.com>
2020-01-23 19:16:13 +0100

    added test.py the skills of characters

--- stats.py
+++ stats.py
@@ -1,7 +1,6 @@
 class Skill(object):
     """Default object to assign skill values as a basis for character and item skill values"""
     def __init__(self, movement=0, weapon=0, ballistic=0, strength=0, toughness=0, wounds=0, initiative=0, actions=0, leadership=0, armoursave=0):
-        self.name = name
         self.movement = movement
         self.weapon = weapon
         self.ballistic = ballistic
--- test.py
+++ test.py
@@ -23,19 +23,19 @@ def test_makeWarband():
         "Elven Wine"
         ])
     WarbandA.herolist = [
-        Character(name="Hero1", race="High Elves", category="Loremaster", skill=[5,4,4,3,3,1,6,1,9,0], abilitylist=[Ability("Excellent Sight"), Ability("Dispel")], inventory=Inventory(itemlist=["Mage staff", "Dagger"])), 
-        Character(name="Hero2", race="High Elves", category="Swordwarden", skill=[5,4,4,3,3,1,6,1,9,0], abilitylist=[Ability("Excellent Sight")], inventory=Inventory(itemlist=["Sword", "Dagger", "Shield", "Light Armour"]))
+        Character(name="Hero1", race="High Elves", category="Loremaster", skill=Skill(5,4,4,3,3,1,6,1,9,0), abilitylist=[Ability("Excellent Sight"), Ability("Dispel")], inventory=Inventory(itemlist=["Mage staff", "Dagger"])), 
+        Character(name="Hero2", race="High Elves", category="Swordwarden", skill=Skill(5,4,4,3,3,1,6,1,9,0), abilitylist=[Ability("Excellent Sight")], inventory=Inventory(itemlist=["Sword", "Dagger", "Shield", "Light Armour"]))
         ]
     WarbandA.squadlist = [
         Squad(name="Spearguard", category="Seaguard", henchmanlist=[
-            Character(name="Spearguard1", race="High Elves", category="Seaguard", skill=[5,4,4,3,3,1,6,1,8,0], inventory=Inventory(itemlist=["Spear"])),
-            Character("Spearguard2", "High Elves", "Seaguard", [5,4,4,3,3,1,6,1,8,0], Inventory(itemlist=["Spear"]))
+            Character(name="Spearguard1", race="High Elves", category="Seaguard", skill=Skill(5,4,4,3,3,1,6,1,8,0), inventory=Inventory(itemlist=["Spear"])),
+            Character("Spearguard2", "High Elves", "Seaguard", Skill(5,4,4,3,3,1,6,1,8,0), Inventory(itemlist=["Spear"]))
         ]), 
         Squad(name="Bladeguard", category="Seaguard", henchmanlist=[
-            Character(name="Bladeguard1", race="High Elves", category="Seaguard", skill=[5,4,4,3,3,1,6,1,8,0], inventory=Inventory(itemlist=["Sword"]))
+            Character(name="Bladeguard1", race="High Elves", category="Seaguard", skill=Skill(5,4,4,3,3,1,6,1,8,0), inventory=Inventory(itemlist=["Sword"]))
         ]), 
         Squad(name="Cadet Archers", category="Cadet", henchmanlist=[
-            Character(name="Cadet1", race="High Elves", category="Cadet", skill=[5,3,3,3,3,1,6,1,7,0], inventory=Inventory(itemlist=["Longbow"]))
+            Character(name="Cadet1", race="High Elves", category="Cadet", skill=Skill(5,3,3,3,3,1,6,1,7,0), inventory=Inventory(itemlist=["Longbow"]))
         ])
         ]
     
@@ -47,29 +47,21 @@ def test_makeWarband():
     print(f"Race: {WarbandA.race}")
     print(f"Gold: {WarbandA.inventory.gold}")
     print(f"Wyrdstones: {WarbandA.inventory.wyrd}")
-    print(f"Inventory:")
-    for item in WarbandA.inventory.itemlist:
-        print(f"- {item}")
-
+    print(f"Inventory:{WarbandA.inventory.itemlist}")
     print("Heroes:")
-    i = 1
     for hero in WarbandA.herolist:
-        print(f"{i}.")
         print(f"Name: {hero.name}")
-        print(f"category: {hero.category}")
-        print(f"skill: {hero.skill}")
-        print(f"Items: {hero.inventory.itemlist}")
-        i += 1
+        print(f" category: {hero.category}")
+        print(f" skill: m:{hero.skill.movement}, w:{hero.skill.weapon}, b:{hero.skill.ballistic}, s:{hero.skill.strength}, t:{hero.skill.toughness}, a:{hero.skill.actions}, i:{hero.skill.initiative}, w:{hero.skill.wounds}, ld:{hero.skill.leadership}, as:{hero.skill.armoursave}")
+        print(f" Items: {hero.inventory.itemlist}")
 
     print("Squads:")
-    i = 1
     for squad in WarbandA.squadlist:
-        print(f"{i}.")
         print(f"Name: {squad.name}")
-        print(f"category: {squad.category}")
-        print(f"items: {squad.henchmanlist[0].inventory.itemlist}")
-        print(f"(total: {squad.get_totalhenchman()})")
-        i += 1
+        print(f" category: {squad.category}")
+        print(f" skill: m:{squad.henchmanlist[0].skill.movement}, w:{squad.henchmanlist[0].skill.weapon}, b:{squad.henchmanlist[0].skill.ballistic}, s:{squad.henchmanlist[0].skill.strength}, t:{squad.henchmanlist[0].skill.toughness}, a:{squad.henchmanlist[0].skill.actions}, i:{squad.henchmanlist[0].skill.initiative}, w:{squad.henchmanlist[0].skill.wounds}, ld:{squad.henchmanlist[0].skill.leadership}, as:{squad.henchmanlist[0].skill.armoursave}")
+        print(f" items: {squad.henchmanlist[0].inventory.itemlist}")
+        print(f" (total: {squad.get_totalhenchman()})")
 
     
 

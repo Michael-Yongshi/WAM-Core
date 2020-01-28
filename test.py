58a42312d9d7b9297d1faab2a7a07a51ac3868e7
MvdBerg <mfvandenberg@outlook.com>
2020-01-28 13:09:13 +0100

    Added henchman abilities to save

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ def print_newline():
     print(" ")
 
 
+# Test references
+
+
 # test warband classes
 def test_makeWarband():
     # Create data
@@ -22,7 +25,8 @@ def test_makeWarband():
             category="Loremaster", 
             skill=Skill(5,4,4,3,3,1,6,1,9,0), 
             abilitylist=[
-                Ability("Excellent Sight"), 
+                Ability("Excellent Sight"),
+                Ability("High Elven Magic") 
                 ], 
             inventory=Inventory(itemlist=[
                 Item(
@@ -31,7 +35,7 @@ def test_makeWarband():
                     skill=Skill(0,0,0,2,0,0,0,0,0,0),
                     abilitylist=[
                         Ability(name="Concussion", description="Stunned also happens on a roll of 2 on the injury roll."),
-                        Ability(name="One or Two handed", description="Strength bonus is 1 if used onehanded, 2 if used two handed. Declare clearly.")
+                        Ability(name="One or Two handed", description="Strength bonus is 1 if used onehanded, 2 if used two handed.")
                         ],
                     desc="Mage Staves of Hoeth are forged from the finest Ithilmar, and are works of art that are truly beautiful to behold. They are usually decorated with gems and other precious materials. Only a Loremaster may use a Mage Staff.",
                     price=20
@@ -40,7 +44,7 @@ def test_makeWarband():
                     name="Dagger", 
                     category="Melee Weapon",
                     abilitylist=[
-                        Ability(name="Cloth Is Armour", description="For daggers even normal clothing is regarded as armour. Add +1 to the enemy`s armour save.")
+                        Ability(name="Trouble With Normal Clothing", description="For daggers even default clothing is regarded as armour. Add +1 to the enemy`s armour save.")
                         ],
                     price=0
                     )
@@ -157,52 +161,52 @@ def test_makeWarband():
     
     # Load warband data
     
+
+    
     # show data
-    print(f"Warband testing")
-    print(f"Name: {wbid.name}")
-    print(f"Race: {wbid.race}")
-    print(f"Gold: {wbid.inventory.gold}")
-    print(f"Wyrdstones: {wbid.inventory.wyrd}")
-    print(f"Inventory:{wbid.inventory.itemlist}")
-    totalheroes = len(wbid.herolist)
-    totalhenchman = 0
-    for s in wbid.squadlist:
-        totalhenchman = totalhenchman + s.get_totalhenchman()
-    totalchars = totalheroes+totalhenchman
-    printtotal = "Number of units: " + str(totalchars)
-    print(printtotal)
-    print_newline()
+    # print(f"Warband testing")
+    # print(f"Name: {wbid.name}")
+    # print(f"Race: {wbid.race}")
+    # print(f"Gold: {wbid.inventory.gold}")
+    # print(f"Wyrdstones: {wbid.inventory.wyrd}")
+    # print(f"Inventory:{wbid.inventory.itemlist}")
+    # totalheroes = len(wbid.herolist)
+    # totalhenchman = 0
+    # for s in wbid.squadlist:
+    #     totalhenchman = totalhenchman + s.get_totalhenchman()
+    # totalchars = totalheroes+totalhenchman
+    # printtotal = "Number of units: " + str(totalchars)
+    # print(printtotal)
+    # print_newline()
 
-    for hero in wbid.herolist:
-        print(f"Name: {hero.name}")
-        print(f" category: {hero.category}")
-        print(f" skill: m:{hero.skill.movement}, w:{hero.skill.weapon}, b:{hero.skill.ballistic}, s:{hero.skill.strength}, t:{hero.skill.toughness}, a:{hero.skill.actions}, i:{hero.skill.initiative}, w:{hero.skill.wounds}, ld:{hero.skill.leadership}, as:{hero.skill.armoursave}")
-        print(f" Items: {hero.inventory.itemlist}")
-    print("Squads:")
-    print_newline
+    # for hero in wbid.herolist:
+    #     print(f"Name: {hero.name}")
+    #     print(f" category: {hero.category}")
+    #     print(f" skill: m:{hero.skill.movement}, w:{hero.skill.weapon}, b:{hero.skill.ballistic}, s:{hero.skill.strength}, t:{hero.skill.toughness}, a:{hero.skill.actions}, i:{hero.skill.initiative}, w:{hero.skill.wounds}, ld:{hero.skill.leadership}, as:{hero.skill.armoursave}")
+    #     print(f" Items: {hero.inventory.itemlist}")
+    # print("Squads:")
+    # print_newline
 
-    for squad in wbid.squadlist:
-        print(f"Name: {squad.name}")
-        print(f" category: {squad.category}")
-        print(f" skill: m:{squad.henchmanlist[0].skill.movement}, w:{squad.henchmanlist[0].skill.weapon}, b:{squad.henchmanlist[0].skill.ballistic}, s:{squad.henchmanlist[0].skill.strength}, t:{squad.henchmanlist[0].skill.toughness}, a:{squad.henchmanlist[0].skill.actions}, i:{squad.henchmanlist[0].skill.initiative}, w:{squad.henchmanlist[0].skill.wounds}, ld:{squad.henchmanlist[0].skill.leadership}, as:{squad.henchmanlist[0].skill.armoursave}")
-        print(f" items: {squad.henchmanlist[0].inventory.itemlist}")
-        print(f" (total: {squad.get_totalhenchman()})")
-
-    # generic get_dict function? get an object and create a dict from it
+    # for squad in wbid.squadlist:
+    #     print(f"Name: {squad.name}")
+    #     print(f" category: {squad.category}")
+    #     print(f" skill: m:{squad.henchmanlist[0].skill.movement}, w:{squad.henchmanlist[0].skill.weapon}, b:{squad.henchmanlist[0].skill.ballistic}, s:{squad.henchmanlist[0].skill.strength}, t:{squad.henchmanlist[0].skill.toughness}, a:{squad.henchmanlist[0].skill.actions}, i:{squad.henchmanlist[0].skill.initiative}, w:{squad.henchmanlist[0].skill.wounds}, ld:{squad.henchmanlist[0].skill.leadership}, as:{squad.henchmanlist[0].skill.armoursave}")
+    #     print(f" items: {squad.henchmanlist[0].inventory.itemlist}")
+    #     print(f" (total: {squad.get_totalhenchman()})")
 
 
-    # Save data
+    # Save warband to JSON file
+        # Todo: generic get_dict function? get an object and create a dict from it
+        # Todo: for saving get rid of the hechman and save their (redundant) info in the squad dict
     filepath = "saves/" + wbid.name + ".json"
 
     datadict = {}
     wbdict = wbid.get_dict()
     datadict.update(wbdict)
-    # save_json(data=datadict, jsonfile=filepath)
 
     print("save warband info")
     invdict = wbid.inventory.get_dict(ref="inventory")
     datadict["Warband"].update(invdict) # add new inventory dict to warband, practically replacing original value of inventory
-    # save_json(data=datadict, jsonfile=filepath)
 
     print("save warband itemlist")
     datadict["Warband"]["inventory"]["itemlist"]={} # change in a dict before setting dict values
@@ -216,13 +220,13 @@ def test_makeWarband():
 
         print("save warband item abilities")
         datadict["Warband"]["inventory"]["itemlist"][itemref]["abilitylist"]={} # change in a dict before setting dict values
-        a = 1
+        ia = 1
         for ability in item.abilitylist:
-            abilityref = "Ability" + str(a) # to make sure it has a unique key
+            abilityref = "Ability" + str(ia) # to make sure it has a unique key
             print(abilityref)
             abilitydict = ability.get_dict(ref=abilityref)
             datadict["Warband"]["inventory"]["itemlist"][itemref]["abilitylist"].update(abilitydict)
-            a += 1
+            ia += 1
 
     print("save herolist")
     datadict["Warband"]["herolist"]={} # change in a dict before setting dict values
@@ -234,8 +238,20 @@ def test_makeWarband():
         datadict["Warband"]["herolist"].update(herodict)
         h += 1
         
+        skilldict = hero.skill.get_dict(ref="skill")
+        datadict["Warband"]["herolist"][heroref].update(skilldict) # add new skill dict, practically replacing original value of inventory
+
+        datadict["Warband"]["herolist"][heroref]["abilitylist"]={} # change in a dict before setting dict values
+        a = 1
+        for ability in hero.abilitylist:
+            abilityref = "Ability" + str(a) # to make sure it has a unique key
+            print(abilityref)
+            abilitydict = ability.get_dict(ref=abilityref)
+            datadict["Warband"]["herolist"][heroref]["abilitylist"].update(abilitydict)
+            a += 1
+
         invdict = hero.inventory.get_dict(ref="inventory")
-        datadict["Warband"]["herolist"][heroref].update(invdict) # add new inventory dict to warband, practically replacing original value of inventory
+        datadict["Warband"]["herolist"][heroref].update(invdict) # add new inventory dict, practically replacing original value of inventory
     
         datadict["Warband"]["herolist"][heroref]["inventory"]["itemlist"]={} # change in a dict before setting dict values
         i = 1
@@ -246,16 +262,23 @@ def test_makeWarband():
             datadict["Warband"]["herolist"][heroref]["inventory"]["itemlist"].update(itemdict)
             i += 1
     
-    # save_json(data=datadict, jsonfile=filepath)
+            datadict["Warband"]["herolist"][heroref]["inventory"]["itemlist"][itemref]["abilitylist"]={} # change in a dict before setting dict values
+            ia = 1
+            for ability in item.abilitylist:
+                abilityref = "Ability" + str(ia) # to make sure it has a unique key
+                print(abilityref)
+                abilitydict = ability.get_dict(ref=abilityref)
+                datadict["Warband"]["herolist"][heroref]["inventory"]["itemlist"][itemref]["abilitylist"].update(abilitydict)
+                ia += 1
     
     datadict["Warband"]["squadlist"]={} # change in a dict before setting dict values
-    s = 1
+    sq = 1
     for squad in wbid.squadlist:
-        squadref = "squad" + str(s) # to make sure it has a unique key
+        squadref = "squad" + str(sq) # to make sure it has a unique key
         print(squadref)
         squaddict = squad.get_dict(ref=squadref)
         datadict["Warband"]["squadlist"].update(squaddict)
-        s += 1
+        sq += 1
 
         datadict["Warband"]["squadlist"][squadref]["henchmanlist"]={} # change in a dict before setting dict values
         h = 1
@@ -266,6 +289,18 @@ def test_makeWarband():
             datadict["Warband"]["squadlist"][squadref]["henchmanlist"].update(henchmandict)
             h += 1
             
+            skilldict = henchman.skill.get_dict(ref="skill")
+            datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref].update(skilldict) # add new skill dict, practically replacing original value of inventory
+
+            datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref]["abilitylist"]={} # change in a dict before setting dict values
+            a = 1
+            for ability in henchman.abilitylist:
+                abilityref = "Ability" + str(a) # to make sure it has a unique key
+                print(abilityref)
+                abilitydict = ability.get_dict(ref=abilityref)
+                datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref]["abilitylist"].update(abilitydict)
+                a += 1
+
             invdict = henchman.inventory.get_dict(ref="inventory")
             datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref].update(invdict) # add new inventory dict to warband, practically replacing original value of inventory
         
@@ -277,7 +312,17 @@ def test_makeWarband():
                 itemdict = item.get_dict(ref=itemref)
                 datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref]["inventory"]["itemlist"].update(itemdict)
                 i += 1
-            
+
+                datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref]["inventory"]["itemlist"][itemref]["abilitylist"]={} # change in a dict before setting dict values
+                ia = 1
+                for ability in item.abilitylist:
+                    abilityref = "Ability" + str(ia) # to make sure it has a unique key
+                    print(abilityref)
+                    abilitydict = ability.get_dict(ref=abilityref)
+                    datadict["Warband"]["squadlist"][squadref]["henchmanlist"][henchmanref]["inventory"]["itemlist"][itemref]["abilitylist"].update(abilitydict)
+                    ia += 1
+
+    print("save completed")
 
     # save to file
     save_json(data=datadict, jsonfile=filepath)

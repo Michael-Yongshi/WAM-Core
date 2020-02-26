dde2d78dc5faeb1a501b1b7e0e7afb69c48c67b5
MvdBerg <mfvandenberg@outlook.com>
2020-02-26 19:23:34 +0100

    Load draft

--- source/methods_json.py
+++ source/methods_json.py
@@ -8,35 +8,39 @@ def open_json(jsonfile):
         data = json.load(infile)
     return data
 
-# dumping data to json
-def save_json(data ,jsonfile):
-
-    with open(jsonfile, 'w') as outfile:
-        json.dump(data, outfile, indent=4)
-
-# adding a record 
-def append_json(data, datatype, jsonfile):
-    with open(jsonfile, 'r') as infile:
-        loaddata = json.load(infile)
-    loaddata[datatype]=data
-    save_json(loaddata, jsonfile)
-
 def save_file(datadict, filename):
     """Dumps a save file to the documents folder"""
 
     # set the paths to the users documents folder
-    path = r"~\WAM"
-    user_path = os.path.expanduser(path)
-    print(user_path)
+    local_path = r"~\WAM"
+    user_path = os.path.expanduser(local_path)
 
     # check if file already exists, if not create it
     if not os.path.exists(user_path):
         os.makedirs(user_path)
 
     complete_path = os.path.join(user_path, filename + ".json")
-    print(complete_path)
 
-    # open file
+    # open file and write json to it
     with open(complete_path, 'w') as savefile:
         # dump json data in the file
         json.dump(datadict, savefile, indent=4)
+
+def load_file(filename):
+    """Load files to the documents folder"""
+
+    # set the paths to the users documents folder
+    local_path = r"~\WAM"
+    user_path = os.path.expanduser(local_path)
+
+    # check if file already exists, if not create it
+    if not os.path.exists(user_path):
+        os.makedirs(user_path)
+
+    complete_path = os.path.join(user_path, filename + ".json")
+
+    # open save file and return the datadict
+    with open(complete_path, 'r') as infile:
+        datadict = json.load(infile)
+    
+    return datadict

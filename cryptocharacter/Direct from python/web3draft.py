1d1365c3f305e7889d8ae9489886759094394cbc
MvdBerg <mfvandenberg@outlook.com>
2020-02-28 13:42:33 +0100

    Works, getting receipt by waiting by default first 10 ms before checking receipt. not checking correctly as it doesnt return a None so breaks the while loop

--- cryptocharacter/Direct from python/web3draft.py
+++ cryptocharacter/Direct from python/web3draft.py
@@ -7,7 +7,7 @@ from solc import compile_files
 
 # set up w3 details
 contract_address     = "0x20589989d97b41ea6b9F748CEB4a17527CaA2C1a"                           # [YOUR CONTRACT ADDRESS]
-wallet_private_key   = "ad5bb5684dbfb337040fb31d76c7b6118e0bb4fed23e940451a43746f93ebb09"     # [YOUR TEST WALLET PRIVATE KEY]
+wallet_private_key   = "0xad5bb5684dbfb337040fb31d76c7b6118e0bb4fed23e940451a43746f93ebb09"     # [YOUR TEST WALLET PRIVATE KEY]
 wallet_address       = "0x980df35116009EB3937B0fD7931E3620114fDb9b"                           # [YOUR WALLET ADDRESS]
 
 w3 = Web3(Web3.HTTPProvider("http://51.105.171.12")) # [YOUR NODE URL]
@@ -22,28 +22,33 @@ def send_ether_to_contract(amount_in_ether):
 
     nonce = w3.eth.getTransactionCount(wallet_address)
 
+    print(nonce)
+
     txn_dict = {
+            'from': wallet_address,
             'to': contract_address,
             'value': amount_in_wei,
-            'gas': 2000000,
-            'gasPrice': w3.toWei('40', 'gwei'),
+            'gas': 100000,
+            'gasPrice': w3.toWei('1000000000', 'wei'),
             'nonce': nonce,
-            'chainId': 3
+            'chainId': 98052,
     }
 
     signed_txn = w3.eth.account.signTransaction(txn_dict, wallet_private_key)
 
     txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
+    time.sleep(10)
+
     txn_receipt = None
     count = 0
-    while txn_receipt is None and (count < 30):
+    while txn_receipt is None and (count < 5):
 
-        txn_receipt = w3.eth.getTransactionReceipt(txn_hash)
+        txn_receipt = w3.eth.getTransactionReceipt(txn_hash.hex())
 
-        print(txn_receipt)
+        count += 1
 
-        time.sleep(10)
+        time.sleep(1)
 
 
     if txn_receipt is None:
@@ -52,4 +57,4 @@ def send_ether_to_contract(amount_in_ether):
     return {'status': 'added', 'txn_receipt': txn_receipt}
 
 if __name__ == "__main__":
-    send_ether_to_contract(1)
+    send_ether_to_contract(0.00005)

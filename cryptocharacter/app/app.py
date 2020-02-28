176b2e0f9fd4366893c1ee8f77bea00b0c4cc7df
MvdBerg <mfvandenberg@outlook.com>
2020-02-28 16:38:01 +0100

    Working, but we have to add new character to character other mapping, otherwise its not returning it

--- cryptocharacter/app/app.py
+++ cryptocharacter/app/app.py
@@ -6,70 +6,87 @@ from solc import compile_files
 from methods_json import load_file
 
 
-def create_cc(wallet_address, wallet_private_key, name, unit, race):
+# set up w3 connection
+w3 = Web3(Web3.HTTPProvider("http://51.105.171.12"))
 
-    # set up w3 connection
-    w3 = Web3(Web3.HTTPProvider("http://51.105.171.12"))
+# the necessary contract address for this contract
+contract_address = "0x20589989d97b41ea6b9F748CEB4a17527CaA2C1a"
 
-    # the necessary contract address for this contract
-    contract_address = "0x20589989d97b41ea6b9F748CEB4a17527CaA2C1a"
+# the needed abi functions for this contract
+abi = load_file("cryptocharacter/abi/", "cc_abi")
 
-    # the needed abi functions for this contract
-    abi = load_file("cryptocharacter/abi/", "cc_abi")
+# set up the contract based on the abi functions and the contract address
+contract = w3.eth.contract(address=contract_address, abi = abi)
 
-    # set up the contract based on the abi functions and the contract address
-    contract = w3.eth.contract(address=contract_address, abi = abi)
 
+
+def create_character(wallet_address, wallet_private_key, name, unit, race):
+
+    # get nonce for txn input
+    nonce = w3.eth.getTransactionCount(wallet_address)
+
+    # get identifier for function input
+    identifier = name + unit + race
+
+    # build transaction
+    txn_dict = contract.functions.createRandomCharacter(
+        identifier, 
+        name, 
+        unit, 
+        race
+        ).buildTransaction({
+        'nonce': nonce,        
+        'from': wallet_address,
+        'value': 0,
+        'gas': 100000,
+        'gasPrice': w3.toWei('1000000000', 'wei'),
+        'chainId': 98052,
+        })
+
+    signed_txn = w3.eth.account.signTransaction(txn_dict, wallet_private_key)
+
+    txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    
+    txn_receipt = None
+
+    print("waiting for nodes to handle txn")
+    time.sleep(30)
+    print("requesting for receipt of txn")
+
+    txn_receipt = w3.eth.getTransactionReceipt(txn_hash.hex())
+
+    #     count += 1
+
+    #     time.sleep(1)
+
+    if txn_receipt is None:
+        return {'status': 'failed', 'error': 'timeout'}
+
+    # return {'status': 'added', 'txn_receipt': txn_receipt}
+
+    return str(name + " " + unit + " " + race)
+
+def get_characters(wallet_address):
     # print all knwon characters of the given wallet_address
-
     characters = contract.functions.getCharactersByOwner(wallet_address).call()
-    print(characters)
+    print("character Ids: " + str(characters))
 
+    characterlist = []
     for i in characters:
-        character = contract.functions.characters(i).call()
-        print(character)
+        charlist = contract.functions.characters(i).call()
+        idlist = [i]
+        character = idlist + charlist
+        characterlist += [character]
 
-        events = contract.functions.getEventsByCharacter(i).call()
-        print(events)
+    return characterlist
 
-        for e in events:
-            event = contract.functions.events(e).call()
-            print(e)
+        # # Print all events of the character
+        # events = contract.functions.getEventsByCharacter(i).call()
+        # print(events)
 
-
-        # nonce = w3.eth.getTransactionCount(wallet_address)
-
-        # txn_dict = {
-        #         'from': wallet_address,
-        #         'to': contract_address,
-        #         'value': 0,
-        #         'gas': 100000,
-        #         'gasPrice': w3.toWei('1000000000', 'wei'),
-        #         'nonce': nonce,
-        #         'chainId': 98052,
-        # }
-
-        # signed_txn = w3.eth.account.signTransaction(txn_dict, wallet_private_key)
-
-        # txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-
-        # time.sleep(10)
-
-        # txn_receipt = None
-        # count = 0
-        # while txn_receipt is None and (count < 5):
-
-        #     txn_receipt = w3.eth.getTransactionReceipt(txn_hash.hex())
-
-        #     count += 1
-
-        #     time.sleep(1)
-
-
-        # if txn_receipt is None:
-        #     return {'status': 'failed', 'error': 'timeout'}
-
-        # return {'status': 'added', 'txn_receipt': txn_receipt}
+        # for e in events:
+        #     event = contract.functions.events(e).call()
+        #     print(e)
 
     def create_event(characterId):
         """create an event for a specific character by sending input to the smart contract of cryptocharacter"""
@@ -81,20 +98,32 @@ def create_cc(wallet_address, wallet_private_key, name, unit, race):
 
 
 if __name__ == "__main__":
-    # set up w3 details
-    mycharacter = create_cc(
-        wallet_address = "0x980df35116009EB3937B0fD7931E3620114fDb9b",
-        wallet_private_key = "0xad5bb5684dbfb337040fb31d76c7b6118e0bb4fed23e940451a43746f93ebb09",
+    wallet_address = "0x980df35116009EB3937B0fD7931E3620114fDb9b"
+    wallet_private_key = "0xad5bb5684dbfb337040fb31d76c7b6118e0bb4fed23e940451a43746f93ebb09"
+    
+    newcharacter = create_character(
+        wallet_address = wallet_address,
+        wallet_private_key = wallet_private_key,
         name = "Michael", 
         unit = "Developer", 
         race = "Pikachu",
     )
-    
+    print("New character: " + str(newcharacter))
 
-    # create_event(
+    # newevent = create_event(
+    #     wallet_address = wallet_address,
+    #     wallet_private_key = wallet_private_key,
     #     characterId = 1,
+    #     description = "Something happened",
     # )
 
-    # get_events(
+    characters = get_characters(
+        wallet_address = wallet_address,
+    )
+    print("Character data: " + str(characters))
+
+    # events = get_events(
+    #     wallet_address = wallet_address,
     #     characterId = 1,
     # )
+    # print(events)

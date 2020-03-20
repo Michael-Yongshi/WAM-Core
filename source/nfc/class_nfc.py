93f89c4ef55ada8d2bfd2c8d1aecd3415b2bb49f
Michael-Yongshi <4registration@outlook.com>
2020-03-20 17:23:49 +0100

    Made an nfc connection class and test nfc

--- source/nfc/class_nfc.py
+++ source/nfc/class_nfc.py
@@ -1,51 +1,144 @@
-import os
-import time
-
-from web3 import Web3
-
-from source.methods_json import load_json
+# https://pyscard.sourceforge.io/user-guide.html
+# Sample script for the card centric approach
+from __future__ import print_function
+from smartcard.ATR import ATR
+from smartcard.CardType import ATRCardType, AnyCardType
+from smartcard.CardRequest import CardRequest
+from smartcard.CardConnection import CardConnection
+from smartcard.util import toHexString, toBytes
 
 class NFCconnection(object):
-    def __init__(self, w3, contract, account):
+    def __init__(self, cardservice):
         super().__init__()
-        self.w3 = w3
-        self.contract = contract
-        self.account = account
-        
+        self.cardservice = cardservice
+       
     @staticmethod
-    def initialize(network_url, wallet_private_key, abi, contract_address="", solidity="", bytecode=""):
-        
-        # Connect to specific network
-        w3 = Web3(Web3.HTTPProvider(network_url))
-        print(w3.isConnected())
+    def initialize(atr, atrhex):
 
-        # account to interact from
-        account = w3.eth.account.privateKeyToAccount(wallet_private_key)
-        print(account.address)
+        # data for the card based on the received ATR (using nfc tools)
+        mycardatr = atr
+        print(f"mycardatr: {mycardatr}")
+        mycardhex = toHexString(atrhex)
+        print(f"mycardhex: {mycardhex}")
+        mycardbytes = toBytes(mycardhex)
+        print(f"mycardbytes: {mycardbytes}")
 
-        if contract_address == "" and bytecode == "" and solidity == "":
+        # Sample script for the smartcard.ATR utility class.
+        atr = ATR(atrhex)
 
-            print("provide valid contract address, solidity contract or bytecode to create a new contract!")
+        print(atr)
+        print('historical bytes: ', toHexString(atr.getHistoricalBytes()))
+        print('checksum: ', "0x%X" % atr.getChecksum())
+        print('checksum OK: ', atr.checksumOK)
+        print('T0  supported: ', atr.isT0Supported())
+        print('T1  supported: ', atr.isT1Supported())
+        print('T15 supported: ', atr.isT15Supported())
 
-        else:
+        # cardtype = AnyCardType() # for accepting any type of card
+        cardtype = ATRCardType(mycardbytes)
+        cardrequest = CardRequest( timeout=1, cardType=cardtype )
 
-            if bytecode != "":
-                # if bytecode is provided, create new contract (address) with provided bytecode
-                contract_address = Web3Connection.init_deploy_bytecode(w3, account, abi, bytecode)
+        print("Waiting for card")
+        cardservice = cardrequest.waitforcard()
+        print("Card connected")
 
-            elif solidity != "":
-                # if solidity contract is provided, create new contract (address) with provided contract
-                contract_address = Web3Connection.init_deploy_solidity(w3, account, abi, solidity)   
-            
-            # Setting up contract with the needed abi (functions) and the contract address (for instantiation)
-            abi = load_json(abi["path"], abi["file"])
-            # print(abi)
-            
-            contract = w3.eth.contract(abi = abi, address = contract_address)
-            print(contract.address)
+        # connecting to card
+        cardservice.connection.connect()
+        # cardservice.connection.connect( CardConnection.T1_protocol) # Connecting with a specific T1 or T2 protocol
 
-            return Web3Connection(
-                w3 = w3,
-                contract = contract,
-                account = account,
-            )
+        print("connected to reader: " + str(cardservice.connection.getReader()))
+        print("connected to card (in bytes): " + str(cardservice.connection.getATR()))
+        print("connected to card (in hex): " + str(toHexString(cardservice.connection.getATR())))
+        print("this is my card") if cardservice.connection.getATR() == mycardbytes else print("this is not my card")
+
+        return NFCconnection(
+            cardservice = cardservice,
+        )
+
+    def read_card(self):
+    
+        # op_type determines what kind of card to be selected???
+        # MF card?
+        # op_type = [
+        #     0x00, # CLS ?
+        #     0xA4, # INS ?
+        #     0x00, # P1 field?
+        #     0x00, # P2 field?
+        #     0x02, # Number of bytes to read
+        #     0x3F, # File id part 1
+        #     0x01, # file id part 2
+        #     ]
+
+        # valid, but dont know what it does
+        # op_type = [
+        #     0xFF,
+        #     0x82,
+        #     0x00,
+        #     0x00,
+        #     0x05,
+        # ]
+
+        # should result in getting uid of card but response is empty (in tutorial SELECT)
+        op_type = [
+            0xFF, 
+            0xCA, 
+            0x00, 
+            0x00,
+            0x00,
+            ]
+
+        # read data
+        # op_type = [
+        #     0x00,
+        #     0xB0,
+        #     0x00,
+        #     0x00,
+        # ]
+
+        # # write data
+        # op_type = [
+        #     0x00,
+        #     0xD0,
+        #     0x00,
+        #     0x00,
+        # ]
+
+        # in order to log the details of the op_type variable we translate the bytes to hex so they become human readable
+        op_typehex = []
+        for i in op_type:
+            hexstring = hex(i)
+            hexstring12 = hexstring[0] + hexstring[1]
+            if len(hexstring) == 3:
+                hexstring34 = hexstring[2].upper() + "0"
+            else:
+                hexstring34 = hexstring[2].upper() + hexstring[3].upper()
+            hexstring = hexstring12 + hexstring34
+            op_typehex += [hexstring]
+
+        print(f"op_type hex: {op_typehex}")
+        print(f"op_type: {op_type}")
+
+        # use the following details (in tutorial DF_TELECOM)
+        op_details = [0x05, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+        # in order to log the details of the op_details variable we translate the bytes to hex so they become human readable
+        op_detailshex = []
+        for i in op_details:
+            hexstring = hex(i)
+            hexstring12 = hexstring[0] + hexstring[1]
+            if len(hexstring) == 3:
+                hexstring34 = hexstring[2].upper() + "0"
+            else:
+                hexstring34 = hexstring[2].upper() + hexstring[3].upper()
+            hexstring = hexstring12 + hexstring34
+            op_detailshex += [hexstring]
+
+        print(f"op_details hex: {op_detailshex}")
+        print(f"op_details: {op_details}")
+
+        apdu = op_type + op_details
+        print(f"sending {toHexString(apdu)}")
+
+        # response, sw1, sw2 = cardservice.connection.transmit( apdu, CardConnection.T1_protocol )
+        response, sw1, sw2 = self.cardservice.connection.transmit( apdu )
+        print(f"response: {response} status words: {sw1} {sw2}")

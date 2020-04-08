9ac49737ba530bb881cdd36b61427a382d591f6b
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-04-08 17:20:16 +0200

    removed formatting test depreciated

--- test_nfc.py
+++ test_nfc.py
@@ -1,33 +1,16 @@
 
-from .lib.wam_library_nfc.source.class_nfc import (
+from lib.wam_library_nfc.source.class_nfc import (
     NFCreference,
     NDEFinterpreter,
     NFCconnection,
 )
 
-from .lib.wam_library_nfc.source.lass_conversions import (
+from lib.wam_library_nfc.source.class_conversions import (
     ConvertingNumbers,
     DecodingCharacter,
     EncodingCharacter,
 )
 
-def test_formatting():
-    startbytes = [85, 203, 230, 191]
-    print(f"Converting {startbytes} to hex and back to bytes")
-    bytes_to_hex = NFCmethods.bytes_to_hex(startbytes)
-    print(f"bytes to hex: {bytes_to_hex}")
-    hex_to_bytes = NFCmethods.hex_to_bytes(bytes_to_hex)
-    print(f"hex to bytes: {hex_to_bytes}")
-    print("")
-
-    starthex = [0xBF, 0xE6, 0xCB, 0x55]
-    starthexliteral = "[0xBF, 0xE6, 0xCB, 0x55]"
-    print(f"Converting {starthexliteral} to bytes and back to hex")
-    hex_to_bytes = NFCmethods.hex_to_bytes(starthex)
-    print(f"hex to bytes: {hex_to_bytes}")
-    bytes_to_hex = NFCmethods.bytes_to_hex(hex_to_bytes)
-    print(f"bytes to hex: {bytes_to_hex}")
-    print("")
 
 if __name__ == '__main__':
 

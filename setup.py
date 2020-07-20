e73ebadaa4cd8bc84350b19a541672f056367d55
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-20 09:09:22 +0200

    added a setup.py for pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="WAM-Core",
+    version="0.1",
+    author="Michael-Yongshi",
+    author_email="4registration@outlook.com",
+    description="WAM-Core package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael-Yongshi/WAM-Core",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)

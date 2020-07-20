ffd28c12419da6df19d95ddaf2db751a747276d4
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-20 15:21:13 +0200

    0.3.8 init in database folder

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="WAM-Core",
-    version="0.3.7",
+    version="0.3.8",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="WAM-Core package",
